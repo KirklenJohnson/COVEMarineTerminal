88df9120841d017f83fdd738e1409e25d5be4d14
KirklenJohnson <kirklen.johnson@coveocean.com>
2026-07-06 12:05:04 -0300

    Update to Map

---
+++ data/BollardsNewInstall_10.js
@@ -0,0 +1,1 @@
+var json_BollardsNewInstall_10 = {"type":"FeatureCollection","name":"BollardsNewInstall_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"xy_coordinate_resolution":1e-15,"features":[]}
